544afb3e5ea6a78f3c19b3e2595e8c66944dda98
balogunVictor <balogunmoyinoluwav@gmail.com>
2024-12-12 18:16:23 +0100

    updated password input

--- src/components/common/PasswordInput.tsx
+++ src/components/common/PasswordInput.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import React, {
   DetailedHTMLProps,
   forwardRef,
@@ -10,6 +8,7 @@ import React, {
 import { ExclamationTriangleIcon } from '@heroicons/react/24/solid';
 import { cva, VariantProps } from 'class-variance-authority';
 import classNames from 'classnames';
+import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa';
 
 const inputStyles = cva(
   'w-full appearance-none rounded-md focus:ring-0 font-normal leading-loose px-3 text-sm',
@@ -26,7 +25,7 @@ const inputStyles = cva(
       intent: {
         fill: 'bg-gray-100 border-gray-200 focus:border-brand-textInput',
         normal:
-          'border-gray-300 border focus:outline-none placeholder:text-gray-200  focus:border-brand-orange-800 bg-transparent',
+          'border-gray-300 border focus:outline-none placeholder:text-transparent focus:border-orange-800 bg-transparent',
       },
       size: {
         lg: 'h-16',
@@ -37,9 +36,11 @@ const inputStyles = cva(
   }
 );
 
-export type InputProps = Omit<VariantProps<typeof inputStyles>, 'error'> & {
+export type PasswordInputProps = Omit<
+  VariantProps<typeof inputStyles>,
+  'error'
+> & {
   label?: string;
-  loading?: boolean;
   error?: string;
   leftIcon?: ReactNode;
   addon?: boolean;
@@ -48,7 +49,7 @@ export type InputProps = Omit<VariantProps<typeof inputStyles>, 'error'> & {
     'size' | 'type'
   >;
 
-export const PasswordInput = forwardRef<HTMLInputElement, InputProps>(
+export const PasswordInput = forwardRef<HTMLInputElement, PasswordInputProps>(
   (
     { label, error, intent, size, addon, className, leftIcon, ...rest },
     ref
@@ -56,57 +57,55 @@ export const PasswordInput = forwardRef<HTMLInputElement, InputProps>(
     const [showPassword, setShowPassword] = useState(false);
 
     return (
-      <label className={classNames('group block', className)}>
-        {label && (
-          <p className="text-brand-textBlack mb-2 block text-sm font-medium leading-6">
-            {label}
-          </p>
-        )}
-        <div className="relative rounded-md">
-          {leftIcon && (
-            <div className="text-brand-textBlack pointer-events-none absolute inset-y-0 left-0 flex   items-center justify-center">
-              {leftIcon}
-            </div>
+      <label className={classNames('relative block', className)}>
+        <input
+          className={classNames(
+            inputStyles({ error: !!error, intent, size }),
+            'peer',
+            {
+              'pl-8': leftIcon,
+              'pr-8': true, // Account for the toggle icon
+            }
           )}
-          <input
-            className={classNames(
-              inputStyles({ error: !!error, intent, size }),
-              {
-                'pl-8': leftIcon,
-              }
-            )}
-            ref={ref}
-            type={showPassword ? 'text' : 'password'}
-            {...rest}
-          />
-
-          <button
-            className="text-brand-textBlack absolute inset-y-0  right-0 z-20 flex items-center justify-center rounded px-4 text-sm font-medium leading-6 hover:opacity-70"
-            onClick={() => setShowPassword((prev) => !prev)}
-            type="button"
-          >
-            {showPassword ? 'Hide password' : 'Show password'}
-          </button>
-        </div>
-
+          ref={ref}
+          type={showPassword ? 'text' : 'password'}
+          placeholder=" "
+          {...rest}
+        />
+        {label && (
+          <span className="absolute left-3 top-2 text-sm text-gray-500 transition-all duration-200 transform scale-75 -translate-y-4 bg-white px-1 peer-placeholder-shown:translate-y-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:text-gray-400 peer-focus:scale-75 peer-focus:-translate-y-4 peer-focus:text-brand-orange-800">
+            {label}
+          </span>
+        )}
+        {leftIcon && (
+          <div className="text-brand-textBlack pointer-events-none absolute inset-y-0 left-0 flex items-center justify-center px-2">
+            {leftIcon}
+          </div>
+        )}
+        <button
+          className="text-brand-textBlack absolute inset-y-0 right-0 z-20 flex items-center justify-center px-4 text-sm font-medium leading-6 hover:opacity-70"
+          onClick={() => setShowPassword((prev) => !prev)}
+          type="button"
+        >
+          {showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
+        </button>
         {error && (
           <p className="mt-2 flex items-center gap-x-1 text-sm text-rose-500">
             <ExclamationTriangleIcon className="h-4 w-4" />
             {error}
           </p>
         )}
-
         {addon && (
-          <div className="mt-2  hidden space-y-2 transition-all duration-100 group-focus-within:block">
+          <div className="mt-2 hidden space-y-2 transition-all duration-100 group-focus-within:block">
             <span className="block text-sm font-semibold text-black">
-              Password requirement
+              Password Requirements
             </span>
             <ul className="text-brand-textBlack list-disc space-y-1 px-4 text-sm">
-              <li>Must have at least 1 lowercase letter</li>
-              <li>Must have at least 1 uppercase letter</li>
-              <li>Must have at least 1 number</li>
-              <li>Must have at least 1 special character</li>
-              <li>Must have 6 characters minimum</li>
+              <li>At least 1 lowercase letter</li>
+              <li>At least 1 uppercase letter</li>
+              <li>At least 1 number</li>
+              <li>At least 1 special character</li>
+              <li>Minimum 6 characters</li>
             </ul>
           </div>
         )}
