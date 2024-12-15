b963311d08749b628942f490bfaf6e4ddf1bc4cf
balogunVictor <balogunmoyinoluwav@gmail.com>
2024-12-15 12:05:15 +0100

    images

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,3 +1,4 @@
 export { default as HostgadaLogo } from './hostgada.png';
 export { default as LocationImage } from './location.png';
 export { default as landingPage } from './landingPage.webp';
+export { default as GreatEmoji } from './Emoji-Great--Streamline-Ultimate.png';
