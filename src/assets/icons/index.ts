2c5d3c7bb578bb0ce63263a04b670d7b1d6838ad
balogunVictor <balogunmoyinoluwav@gmail.com>
2025-01-11 07:36:55 +0100

    host dashboard

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -21,3 +21,5 @@ export { default as TotalEventsIcon } from './TotalEvents.svg';
 export { default as EmptyEventIcon } from './emptyEvent.svg';
 export { default as TrendingDownIcon } from './ic-trending-down-24px.svg';
 export { default as TrendingUpIcon } from './ic-trending-up-24px.svg';
+export { default as CheckBoxIcon } from './CHECKBOXES.svg';
+export { default as EmptyBookingIcon } from './emptybooking.svg';
